190f060c232f33db5952405b6821726b828b178a
junzhuo <junzhuo22@g.ucla.edu>
2024-10-12 18:35:41 -0700

    rename to bpspice

--- BPspice-py/src/lib.rs
+++ BPspice-py/src/lib.rs
@@ -3,13 +3,13 @@ use pyo3::prelude::*;
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn add(a: usize, b: usize) -> PyResult<usize> {
-    Ok(BPspice::add(a, b))
+    Ok(bpspice::add(a, b))
 }
 
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
-#[pymodule(name = "BPspice")]
+#[pymodule(name = "bpspice")]
 fn pymodule(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(add, m)?)?;
     Ok(())
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# BPspice
+# BPspice: Gradient <ins>B</ins>ackward <ins>P</ins>ropagation <ins>SPICE</ins> simulator
 
 TODO
